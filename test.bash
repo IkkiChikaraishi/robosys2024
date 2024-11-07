b3be347d7625233d1af84088278b9da37662345f
Ikki Chikaraishi <s23c1090ew@s.chibakoudai.jp>
2024-11-07 12:04:36 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -10,12 +10,13 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
+### hennanov###
 out=$(echo あ| ./plus)
-[ "$?" =1 ] || ng "$LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo | ./plus)
-[ "$?" =1 ] || ng "$LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
