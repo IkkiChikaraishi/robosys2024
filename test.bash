a76a065be6c46adf5d54250d7238a23e1fe8952f
Ikki Chikaraishi <s23c1090ew@s.chibakoudai.jp>
2024-11-30 02:01:56 +0900

    test

--- test.bash
+++ test.bash
@@ -11,43 +11,43 @@ out=$(seq 10 | ./plus)
 [ "${out}" = 55 ] || ng "$LINENO"
 
 ### hennanov###
-out=$(echo 1 | ./kadai1)
+out=$(echo 1 | ./kukunohyou)
 [[ "$out" = *"1×1=1"* ]] || ng "$LINENO"
 
-out=$(echo 2 | ./kadai1)
+out=$(echo 2 | ./kukunohyou)
 [[ "$out" = *"2×1=2"* ]] || ng "$LINENO"
 
-out=$(echo 3 | ./kadai1)
+out=$(echo 3 | ./kukunohyou)
 [[ "$out" = *"3×1=3"* ]] || ng "$LINENO"
 
-out=$(echo 4 | ./kadai1)
+out=$(echo 4 | ./kukunohyou)
 [[ "$out" = *"4×1=4"* ]] || ng "$LINENO"
 
-out=$(echo 5 | ./kadai1)
+out=$(echo 5 | ./kukunohyou)
 [[ "$out" = *"5×1=5"* ]] || ng "$LINENO"
 
-out=$(echo 6 | ./kadai1)
+out=$(echo 6 | ./kukunohyou)
 [[ "$out" = *"6×1=6"* ]] || ng "$LINENO"
 
-out=$(echo 7 | ./kadai1)
+out=$(echo 7 | ./kukunohyou)
 [[ "$out" = *"7×1=7"* ]] || ng "$LINENO"
 
-out=$(echo 8 | ./kadai1)
+out=$(echo 8 | ./kukunohyou)
 [[ "$out" = *"8×1=8"* ]] || ng "$LINENO"
 
-out=$(echo 9 | ./kadai1)
+out=$(echo 9 | ./kukunohyou)
 [[ "$out" = *"9×1=9"* ]] || ng "$LINENO"
 
-out=$(echo a | ./kadai1)
+out=$(echo a | ./kukunohyou)
 [[ "$out" = *"エラー: 数字を入力してください。"* ]] || ng "$LINENO"
 
-out=$(echo ab | ./kadai1)
+out=$(echo ab | ./kukunohyou)
 [[ "$out" = *"エラー: 数字を入力してください。"* ]] || ng "$LINENO"
 
-out=$(echo 0 | ./kadai1)
+out=$(echo 0 | ./kukunohyou)
 [[ "$out" = *"エラー: 段の数は1から9の範囲で指定してください。"* ]] || ng "$LINENO"
 
-out=$(echo 10 | ./kadai1)
+out=$(echo 10 | ./kukunohyou)
 [[ "$out" = *"エラー: 段の数は1から9の範囲で指定してください。"* ]] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
