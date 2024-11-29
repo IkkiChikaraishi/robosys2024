78c2760192880e63e7f5149063310a79958c56ca
Ikki Chikaraishi <s23c1090ew@s.chibakoudai.jp>
2024-11-30 02:14:01 +0900

    test

--- test.bash
+++ test.bash
@@ -38,17 +38,17 @@ out=$(echo 8 | ./kukunohyou)
 out=$(echo 9 | ./kukunohyou)
 [[ "$out" = *"9×1=9"* ]] || ng "$LINENO"
 
-#out=$(echo a | ./kukunohyou)
-#[[ "$out" = *"エラー: 数字を入力してください。"* ]] || ng "$LINENO"
+out=$(echo a | ./kukunohyou)
+[[ "$2" = *"エラー: 数字を入力してください。"* ]] || ng "$LINENO"
 
-#out=$(echo ab | ./kukunohyou)
-#[[ "$out" = *"エラー: 数字を入力してください。"* ]] || ng "$LINENO"
+out=$(echo ab | ./kukunohyou)
+[[ "$2" = *"エラー: 数字を入力してください。"* ]] || ng "$LINENO"
 
-#out=$(echo 0 | ./kukunohyou)
-#[[ "$out" = *"エラー: 段の数は1から9の範囲で指定してください。"* ]] || ng "$LINENO"
+out=$(echo 0 | ./kukunohyou)
+[[ "$2" = *"エラー: 段の数は1から9の範囲で指定してください。"* ]] || ng "$LINENO"
 
-#out=$(echo 10 | ./kukunohyou)
-#[[ "$out" = *"エラー: 段の数は1から9の範囲で指定してください。"* ]] || ng "$LINENO"
+out=$(echo 10 | ./kukunohyou)
+[[ "$2" = *"エラー: 段の数は1から9の範囲で指定してください。"* ]] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
